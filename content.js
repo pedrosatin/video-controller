90369335a3e322fe018cba872662350de513a42c
pedrosatin <33128737+pedrosatin@users.noreply.github.com>
2026-09-11 13:00:36 +0000

    🧪 Add test coverage for updateLoopBtn

--- content.js
+++ content.js
@@ -1048,6 +1048,7 @@
       showIndicatorEl,
       hideIndicatorEl,
       _getIndicator: () => indicator,
+      updateLoopBtn,
       applyEnabled,
       promoteToTopLayer,
       videoSummaries,
